ca78fac20d2ea349248430c8a9a377279c4c47b8
konrad.oglaza <konrad.oglaza@schibsted.com>
2024-03-06 16:36:30 +0100

    WebApiProject - solucja demonstrująca użycie Controller-based API

---
+++ LAB02/WebApiProject/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiProject
+{
+    public class Student
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
